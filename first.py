042000bc105606ffd7d73af3c8f45da73ae23458
Richie Redoblado <robertochaming.gmail.com>
2024-03-04 23:32:12 +0800

    Py Commit

---
+++ first.py
@@ -0,0 +1,55 @@
+print("hello")
+print()
+print()
+# Implicit line joining method
+    # Using curly braces
+days = {'Mon', 'Tue', 'Wed',
+        'Thu', 'Fri', 'Sat',
+        'Sun'}
+print(days)
+print()
+ # Using curly brackets
+days = ['Mon', 'Tue', 'Wed',
+        'Thu', 'Fri', 'Sat',
+        'Sun']
+print(days)
+print()
+# Using parenthesis
+days = ('Mon', 'Tue', 'Wed',
+        'Thu', 'Fri', 'Sat',
+        'Sun')
+print(days)
+print()
+print()
+# Explicit joining method
+print("hello\
+        welcome beginners")
+print()
+x = 12
+print(x>10 | \
+      x<9)
+print()
+x, y, z = 3, 4, 5
+print('The ratio of x,y and z are ', x, y,z,sep = ':')
+print()
+print('Hello',end=' ')
+print('Python',end=' ')
+print('Beginners.',end=' ')
+print('This',end=' ')
+print('is',end=' ')
+print('an',end=' ')
+print('example',end=' ')
+print('of',end=' ')
+print('end',end=' ')
+print('attribute.',end=' ')
+print()
+print()
+li = [1, 2, 3, 4]  # list
+t1 = (5, 6, 7, 8)  # tuple
+s1 = {9, 10, 11, 12}  # set
+# Method-1
+print(li, t1, s1)
+# Method-2
+print(li, end=' ')
+print(t1, end=' ')
+print(s1)
---
+++ python_keywords.py
@@ -0,0 +1,5 @@
+import keyword
+print(keyword.kwlist) # to print all python keywords and reserve words
+print()
+print(keyword.iskeyword('rich'))  # To check if any word is a keyword or not
+
